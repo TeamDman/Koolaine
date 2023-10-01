090ea0f344221f56afccc2b70bee05468692d831
TeamDman <TeamDman9201@gmail.com>
2023-10-01 12:32:22 -0400

    fix colours

--- experiments/zoomy/src/main.rs
+++ experiments/zoomy/src/main.rs
@@ -14,9 +14,10 @@ fn main() {
 
     let mut window = Window::new(
         "Zoom Window",
-        CAPTURE_SIZE * ZOOM_FACTOR,
-        CAPTURE_SIZE * ZOOM_FACTOR,
+        100,
+        100,
         WindowOptions {
+            resize: true,
             scale: Scale::X4,
             scale_mode: ScaleMode::Stretch,
             ..Default::default()
@@ -31,29 +32,29 @@ fn main() {
         let x_u16 = x as u16;
         let y_u16 = y as u16;
         let capture_size_u16 = CAPTURE_SIZE as u16;
-        
+
         let (screen_width, screen_height) = screen::size();
 
         let x_left = (x_u16 as i32 - (capture_size_u16 / 2) as i32).max(0) as u16;
         let y_top = (y_u16 as i32 - (capture_size_u16 / 2) as i32).max(0) as u16;
-        
+
         let x_right = (x_left as u32 + capture_size_u16 as u32).min(screen_width as u32) as u16;
         let y_bottom = (y_top as u32 + capture_size_u16 as u32).min(screen_height as u32) as u16;
-        
+
         let adjusted_width = (x_right - x_left).max(1);
         let adjusted_height = (y_bottom - y_top).max(1);
-        
+
         let region = screen::screenshot(x_left, y_top, adjusted_width, adjusted_height);
-        
 
         // Convert the frame data to a buffer compatible with minifb
         if let image::DynamicImage::ImageRgba8(image) = region {
             for (x, y, pixel) in image.enumerate_pixels() {
                 let index = y as usize * CAPTURE_SIZE + x as usize;
-                buffer[index] = ((pixel[3] as u32) << 24) | // A
-                                ((pixel[2] as u32) << 16) | // B
-                                ((pixel[1] as u32) << 8)  | // G
-                                (pixel[0] as u32);         // R
+                buffer[index] = ((pixel[0] as u32) << 16) | // R
+                    ((pixel[1] as u32) << 8) | // G
+                    ((pixel[2] as u32) << 0);
+                //   | // B
+                // (pixel[3] as u32); // A
             }
         }
 
