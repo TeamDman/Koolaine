2bf31f0cefa8d7edbbb69971a27577fc60f9688a
TeamDman <TeamDman9201@gmail.com>
2023-10-01 22:22:22 -0400

    working, woot!

--- experiments/zoomy/src/main.rs
+++ experiments/zoomy/src/main.rs
@@ -78,38 +78,40 @@ fn main() {
                 x2: world_capture_region.x2 - screen.display_info.x,
                 y2: world_capture_region.y2 - screen.display_info.y,
             };
-            // // Step 1: Calculate how much of the capture box is outside the monitor
-            let right_overflow =
-                std::cmp::max(0, screen.display_info.x - screen_capture_region.x1) as u32;
-            let bottom_overflow =
-                std::cmp::max(0, screen.display_info.y - screen_capture_region.y1) as u32;
+            let mut screen_capture_region_inbounds = Region {
+                x1: 0.max(screen_capture_region.x1),
+                y1: 0.max(screen_capture_region.y1),
+                x2: (screen.display_info.width as i32).min(screen_capture_region.x2),
+                y2: (screen.display_info.height as i32).min(screen_capture_region.y2),
+            };
 
-            // // Step 2: Use the overflow to calculate the offset for writing into the buffer
-            let buffer_x_offset = right_overflow as usize;
-            let buffer_y_offset = bottom_overflow as usize;
-
-            print!("{:?}\t", screen.display_info.id);
-            print!("scr: {:?}\t", screen_capture_region);
-            print!("rightover: {:?}\t", right_overflow);
-            print!("botover: {:?}\t", bottom_overflow);
-            print!("bxo: {:?}\t", buffer_x_offset);
-            print!("byo: {:?}\t", buffer_y_offset);
+            print!(
+                "ID {:<10} sx {:<10} sy {:<10} ",
+                screen.display_info.id, screen.display_info.x, screen.display_info.y,
+            );
 
             match screen_capture_region.capture(screen) {
                 Ok(image) => {
+                    let x_offset = screen_capture_region_inbounds.x1 - screen_capture_region.x1;
+                    let y_offset = screen_capture_region_inbounds.y1 - screen_capture_region.y1;
+
                     for (x, y, pixel) in image.enumerate_pixels() {
-                        let mut x_img = x  as u32;// + (CAPTURE_SIZE as i32 - image.width() as i32) as u32;
-                        let mut y_img = y  + (CAPTURE_SIZE as i32 - image.height() as i32) as u32;
+                        let mut x_img = x as i32 + x_offset;
+                        let mut y_img = y as i32 + y_offset;
                         fn clamp<T: Ord>(value: T, min: T, max: T) -> T {
                             std::cmp::max(min, std::cmp::min(value, max))
                         }
-                        x_img = clamp(x_img, 0, CAPTURE_SIZE as u32 - 1);
-                        y_img = clamp(y_img, 0, CAPTURE_SIZE as u32 - 1);
-                        img_buffer.put_pixel(x_img, y_img, Rgba([pixel[0], pixel[1], pixel[2], 255]), );
+                        x_img = clamp(x_img, 0, CAPTURE_SIZE as i32 - 1);
+                        y_img = clamp(y_img, 0, CAPTURE_SIZE as i32 - 1);
+                        img_buffer.put_pixel(
+                            x_img as u32,
+                            y_img as u32,
+                            Rgba([pixel[0], pixel[1], pixel[2], 255]),
+                        );
                     }
                 }
                 Err(e) => {
-                    print!("Error: {}\t", e);
+                    print!("Error: {} ", e);
                 }
             }
 
